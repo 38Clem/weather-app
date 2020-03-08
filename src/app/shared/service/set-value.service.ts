4c6f8726a63026eb72de927076aa110c5abd4449
Clemence <38Clem@users.noreply.github.com>
2020-03-08 14:38:10 +0100

    JsonBin.ts delete
    Use LocalStorage instead of JsonBin, file not needed anymore

--- src/app/shared/service/set-value.service.ts
+++ src/app/shared/service/set-value.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { City } from '../model/city';
 import { Forecast } from '../model/forecast';
-import { JsonBin } from 'src/environments/jsonbin';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 
 @Injectable({
