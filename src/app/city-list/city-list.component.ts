a2804227218845017980d2681d54c0952c02724a
Clemence <38Clem@users.noreply.github.com>
2020-03-08 14:37:16 +0100

    Add ViewChild in CityList Component + Add condition of display for the list
    Now the list will be display only if there is a value and user can get the weather (curent and forecast) of a city in the list by clicking the name of this city

--- src/app/city-list/city-list.component.ts
+++ src/app/city-list/city-list.component.ts
@@ -1,27 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { SetValueService } from '../shared/service/set-value.service';
+import { SearchbarComponent } from './searchbar/searchbar.component';
 
 @Component({
   selector: 'app-city-list',
   templateUrl: './city-list.component.html',
   styleUrls: ['./city-list.component.scss'],
 })
+
 export class CityListComponent implements OnInit {
-  private cityList:[]
+
+  @ViewChild(SearchbarComponent, { static: false }) child: SearchbarComponent;
+
+  private cityList: [];
+
   constructor(
-    private setValue:SetValueService
+    private setValue: SetValueService
   ) {
 
+
+
     setValue.displayCityList()
-    .then((newList:[])=>{
-      this.cityList = newList
-    })
-   }
+      .then((newList: []) => {
+        this.cityList = newList
+      })
+  }
 
-   public delete(index){
-     this.setValue.deleteCity(index)
-   }
+  public getWeather(city) {
+    if (this.child) {
+      this.child.search(city);
+    }
+    else {
+      console.log('PAS DE CHILD');
+    }
+  }
 
-  ngOnInit() {}
+  public delete(index) {
+    this.setValue.deleteCity(index)
+  }
+
+  ngOnInit() { }
 
 }
