25815fa17a006fe6fee43eb1f808979ac0a33bf1
Clemence <38Clem@users.noreply.github.com>
2020-03-08 14:40:15 +0100

    Add new property to api.ts

--- src/environments/api.ts
+++ src/environments/api.ts
@@ -1,5 +1,5 @@
 export const Api = {
     path : `https://api.openweathermap.org/data/2.5/`,
-    key: '&appid=0b1f386a167a0699a14e19fecbcad27c'
-    // key : "1e8c1dade7f7c7c64b662a908dc7b03c";
+    key: '&appid=0b1f386a167a0699a14e19fecbcad27c',
+    units: '&units=metric',
 };
